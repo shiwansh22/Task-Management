5ca9f1fe3a15f361df1f186ad0233e2de6c66aa1
Shiwansh Tripathi <135999680+shiwansh22@users.noreply.github.com>
2026-02-02 00:41:39 +0530

    Final backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,26 @@
-import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import authRoutes from "./modules/auth/auth.routes";
-import taskRoutes from "./modules/tasks/task.routes";
-import { errorHandler } from "./middlewares/error.middleware";
 import cookieParser from "cookie-parser";
 
-
+import authRoutes from "./modules/auth/auth.routes";
+import taskRoutes from "./modules/tasks/task.routes";
+import errorMiddleware from "./middlewares/error.middleware";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
-app.use(errorHandler);
-app.use(cookieParser());
+app.use(errorMiddleware);
+
 export default app;
